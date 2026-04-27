5c27bb638ab67d430da1f1b09a461e6c0c1796ee
Abdul Rahim Shahad <74269923+rahimshahad@users.noreply.github.com>
2026-04-27 06:04:50 -0400

    basic home route and search route skeleton

--- data/buildings.ts
+++ data/buildings.ts
@@ -1,4 +1,4 @@
-import { buildings } from '../data/config/mongoCollections.js';
+import { buildings } from './config/mongoCollections.js';
 
 export const getAllBuildings = async () => {
   const buildingCollection = await buildings();
--- routes/search.ts
+++ routes/search.ts
@@ -1,10 +1,15 @@
 import { Router } from 'express';
-import { ObjectId } from 'mongodb';
+// import { ObjectId } from 'mongodb';
+import { getAllBuildings } from '../data/buildings.js';
 
 const router = Router();
 
 //landing page code
+router.get('/', async (req, res) => {
+  res.render('home', { title: 'Before You Sign' });
+});
 
 //search results page code
+router.get('/search', async (req, res) => {});
 
 export default router;
