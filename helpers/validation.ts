07b510bd026a1f1a19f254d2011571fc2d880612
MasterDefault <delriofocesvictorjavier@gmail.com>
2026-05-09 16:31:52 -0400

    seeddb

--- helpers/validation.ts
+++ helpers/validation.ts
@@ -1,3 +1,4 @@
+import * as z from "zod";
 export const checkString = (
   str: string,
   minLength: number,
@@ -5,7 +6,7 @@ export const checkString = (
   invalidChars: RegExp,
 ) => {
   str = str.trim();
-  if (typeof str !== 'string') {
+  if (typeof str !== "string") {
     throw `Error: ${str} must be a string.`;
   }
   if (str.length === 0) {
@@ -23,7 +24,6 @@ export const checkString = (
   if (invalidChars.test(str)) {
     throw `Error: ${str} constains invalid characters.`;
   }
-import * as z from 'zod';
 
   return str;
 };
