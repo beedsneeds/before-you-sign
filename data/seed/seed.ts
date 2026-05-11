d8db43f0549730922c3dd6da62e6c9d179f6c5f0
Bharadhwaj Ram <bharadhwajkram@gmail.com>
2026-05-10 22:02:03 -0400

    Fixed hardcoded stats

--- data/seed/seed.ts
+++ data/seed/seed.ts
@@ -6,11 +6,11 @@ import { findNewBuildings } from "../cron/findNewBuildings.js";
 import { ingestViolations } from "../cron/ingestViolations.js";
 import { UserModel } from "../models/User.js";
 import { BuildingModel } from "../models/Building.js";
-import { ReviewModel } from "../models/Review.js";
+import { addReview } from "../reviews.js";
+import { addComment } from "../comments.js";
+import { addReply } from "../replies.js";
 
 //REMEMBER, comments are now called topics in "Forum" in buildings
-import { CommentModel } from "../models/Comment.js";
-import { ReplyModel } from "../models/Reply.js";
 
 import { existsSync } from "node:fs";
 import { join } from "node:path";
@@ -32,18 +32,6 @@ const main = async () => {
   const building3Id = new Types.ObjectId();
   const assocBuildId = new Types.ObjectId();
 
-  const review1Id = new Types.ObjectId();
-  const review2Id = new Types.ObjectId();
-  const review3Id = new Types.ObjectId();
-
-  // "Comment" model is now "Topics" (in name only)
-  const topic1Id = new Types.ObjectId();
-  const topic2Id = new Types.ObjectId();
-  const topic3Id = new Types.ObjectId();
-
-  const reply1Id = new Types.ObjectId();
-  const reply2Id = new Types.ObjectId();
-
   await UserModel.insertMany([
     {
       _id: adminId,
@@ -52,7 +40,7 @@ const main = async () => {
       email: "sudo@gmail.com",
       hashedPassword: await hashPassword("superpassword"),
       isAdmin: true,
-      activityScore: 10,
+      activityScore: 0,
       savedBuildings: [building1Id, building2Id],
       notificationPrefs: ["email", "inApp"],
     },
@@ -63,7 +51,7 @@ const main = async () => {
       email: "normal@gmail.com",
       hashedPassword: await hashPassword("normalpassword"),
       isAdmin: false,
-      activityScore: 5,
+      activityScore: 0,
       savedBuildings: [building2Id],
       notificationPrefs: ["inApp"],
     },
@@ -75,6 +63,7 @@ const main = async () => {
       email: "mayor@zohranfornyc.com",
       hashedPassword: await hashPassword("imthemayor"),
       isAdmin: true,
+      activityScore: 0,
 
       savedBuildings: [],
     },
@@ -85,15 +74,11 @@ const main = async () => {
       _id: building1Id,
       address: "179-49 Zoller Road, Queens, NY 11434",
       BIN: 4269660,
-      //avgRating: 4,
-      //reviewsCount: 1,
     },
     {
       _id: building2Id,
       address: "178-26 Zoller Road, Queens, NY 11434",
       BIN: 4270016,
-      //avgRating: 3,
-      //reviewsCount: 1,
     },
     {
       _id: building3Id,
@@ -103,7 +88,7 @@ const main = async () => {
       //reviewsCount: 0,
     },
 
-    // BUILDING WITH LOTS OF ASSOCIATED BUILDINGS
+    // BUILDING WITH ASSOCIATED BUILDINGS
     //instructions in main README
     {
       _id: assocBuildId,
@@ -112,79 +97,33 @@ const main = async () => {
     },
   ]);
 
-  await ReviewModel.insertMany([
-    {
-      _id: review1Id,
-      userId: adminId,
-      buildingId: building1Id,
-      rating: 4,
-      reviewText: "Great location, but maintenance is slow.",
-      timeCreated: new Date(),
-    },
-    {
-      _id: review2Id,
-      userId: userId,
-      buildingId: building2Id,
-      rating: 3,
-      reviewText: "Decent place but recurring issues.",
-      timeCreated: new Date(),
-    },
-    {
-      _id: review3Id,
-      userId: zm_userId,
-      buildingId: building2Id,
-      rating: 3,
-      reviewText: "The location is amazing, but it's very crowded.",
-      timeCreated: new Date(),
-    },
-  ]);
+  // Use business logic functions to compute building ratings and user activity scores
+  await addReview(building1Id, "Great location, but maintenance is slow.", 4, adminId);
+  await addReview(building2Id, "Decent place but recurring issues.", 2, userId);
+  await addReview(building2Id, "The location is amazing, but it's very crowded.", 4, zm_userId);
 
-  await CommentModel.insertMany([
-    {
-      _id: topic1Id,
-      userId: userId,
-      buildingId: building1Id,
-      //reviewId: review1Id,
-      topicTitle: "Did anyone else have no hot water for a week?",
-      timeCreated: new Date(),
-    },
-    {
-      _id: topic2Id,
-      userId: adminId,
-      buildingId: building2Id,
-      //reviewId: review2Id,
-      topicTitle: "How much was your rent increase?",
-      timeCreated: new Date(),
-    },
-    {
-      _id: topic3Id,
-      userId: zm_userId,
-      buildingId: building2Id,
-      //reviewId: review2Id,
-      topicTitle: "Did anyone else have heating issues?",
-      timeCreated: new Date(),
-    },
-  ]);
+  const t1 = await addComment(building1Id, "Did anyone else have no hot water for a week?", userId);
+  const t2 = await addComment(building2Id, "How much was your rent increase?", adminId);
+  await addComment(building2Id, "Did anyone else have heating issues?", zm_userId);
 
-  await ReplyModel.insertMany([
-    {
-      _id: reply1Id,
-      topicId: topic1Id,
-      userId: adminId,
-      replyText: "Yes, ours was out for 5 days, had to call 311 until landlord responded.",
-      timeCreated: new Date(),
-    },
-    {
-      _id: reply2Id,
-      topicId: topic2Id,
-      userId: zm_userId,
-      replyText: "Yes, they increased it almost 6%...",
-      timeCreated: new Date(),
-    },
-  ]);
+  await addReply(
+    t1._id,
+    "Yes, ours was out for 5 days, had to call 311 until landlord responded.",
+    adminId,
+  );
+    await addReply(
+    t1._id,
+    "What was worse was I didn't have heating for a week during the worst weather we've seen in decades",
+    adminId,
+  );
+  await addReply(t2._id, "Yes, they increased it almost 6%", zm_userId);
+  await addReply(t2._id, "They don't tax the rich enough!!!!", zm_userId);
+  await addReply(t2._id, "Affordable housing, that's my campaign promise", zm_userId);
+
+
 
   console.log(
-    "To test associated buildings, search BIN 1005521 (requires NYC.2025-5-1.to.2026-5-1.zip from releases. Rename to violations.csv and place in data/cron/, see cron README)",
+    "To test associated buildings, search BIN 199995 or 1005521 (requires NYC.2025-5-1.to.2026-5-1.zip from releases. Rename to violations.csv and place in data/cron/, see cron README)",
   );
 
   if (existsSync(CSV_PATH)) {
